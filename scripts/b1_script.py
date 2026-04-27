58047fa5a9aa12c026742526c65725520d92447d
U220709704 <kamalmialqedra@posta.mu.edu.tr>
2026-04-27 19:47:08 +0300

    update script

--- scripts/b1_script.py
+++ scripts/b1_script.py
@@ -2,16 +2,16 @@ import torch
 import torch.nn as nn
 from torch.optim import AdamW
 
-from data import get_data_loader
-from data import get_transform
-from models import B1Model
-from utils import train, full_evaluation
+from data.data_loader import get_data_loader
+from data.transformers import get_transform
+from models.b1_model import B1Model
+from utils.evaluator import full_evaluation
+from utils.trainer import train
 
 device = torch.device("cuda" if torch.cuda.is_available() else "cpu")
 pkl_path = "/kaggle/input/datasets/sherif31/group-activity-recognition-volleyball/annot_all.pkl"
 videos_path = "/kaggle/input/datasets/sherif31/group-activity-recognition-volleyball/videos"
 save = "/kaggle/working/"
-mode = "frame"
 batch_size = 64
 num_workers = 4
 lr = 1e-3
@@ -24,7 +24,7 @@ frame_transform, crop_transform = get_transform()
 train_loader, val_loader, test_loader = get_data_loader(
     pkl_path=pkl_path,
     videos_path=videos_path,
-    mode=mode,
+    mode="frame",
     frame_transform=frame_transform,
     batch_size=batch_size,
     num_workers=num_workers,
