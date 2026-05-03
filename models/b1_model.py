e25ff34137c494bb7da33284cf06614049f1cdde
U220709704 <kamalmialqedra@posta.mu.edu.tr>
2026-05-04 00:18:37 +0300

    B1model improvments

--- models/b1_model.py
+++ models/b1_model.py
@@ -6,16 +6,7 @@ class B1Model(nn.Module):
     def __init__(self, num_classes=8):
         super(B1Model, self).__init__()
         self.model = models.resnet50(weights='IMAGENET1K_V2')
-        in_features = self.model.fc.in_features
-
-        self.model.fc = nn.Sequential(
-            nn.Linear(in_features, 512),
-            nn.BatchNorm1d(512),
-            nn.ReLU(),
-            nn.Dropout(p=0.5),
-            nn.Linear(512, num_classes)
-        )
+        self.model.fc = nn.Linear(self.model.fc.in_features, num_classes)
 
     def forward(self, x):
-        x = self.model(x)
-        return x
+        return self.model(x)
