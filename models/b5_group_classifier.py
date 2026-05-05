dd1b54d21d4845790f4df9b4dae2eeda8e7abe8d
U220709704 <kamalmialqedra@posta.mu.edu.tr>
2026-05-05 18:29:27 +0300

    fixed a bug in the tensor shape

--- models/b5_group_classifier.py
+++ models/b5_group_classifier.py
@@ -1,14 +1,15 @@
+import torch
 from torch import nn
-
 from models import B5Model
 
 
 class B5BModel(nn.Module):
     def __init__(self, player_model: B5Model, hidden_size=512, num_classes=8, freeze_backbone=False):
         super(B5BModel, self).__init__()
+        self.freeze_backbone = freeze_backbone
         self.player_model = player_model
 
-        if freeze_backbone:
+        if self.freeze_backbone:
             for param in self.player_model.parameters():
                 param.requires_grad = False
 
