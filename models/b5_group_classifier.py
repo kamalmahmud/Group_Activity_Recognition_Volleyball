34c32edef6e89d701bdfd6f2f96bf613ea27ff6c
U220709704 <kamalmialqedra@posta.mu.edu.tr>
2026-05-08 14:13:17 +0300

    batch size

--- models/b5_group_classifier.py
+++ models/b5_group_classifier.py
@@ -14,10 +14,7 @@ class B5BModel(nn.Module):
                 param.requires_grad = False
 
         self.group_classifier = nn.Sequential(
-            nn.Linear(512, 256),
-            nn.ReLU(),
-            nn.Dropout(0.3),
-            nn.Linear(256, 128),
+            nn.Linear(512, 128),
             nn.ReLU(),
             nn.Dropout(0.3),
             nn.Linear(128, num_classes)
