ee4d022dc5fe323315981bd5b28e64ce4ecac03a
U220709704 <kamalmialqedra@posta.mu.edu.tr>
2026-05-12 01:52:57 +0300

    b7 training

--- scripts/b7_script.py
+++ scripts/b7_script.py
@@ -12,7 +12,7 @@ from utils.trainer import train
 
 checkpoint_path = "/kaggle/input/models/kamalalqedra/temporal-player-action/pytorch/default/1/best_model.pth"
 lr = 1e-3
-batch_size = 16
+batch_size = 8
 num_workers = 4
 CLASS_NAMES = list(GROUP_LABELS.keys())
 
