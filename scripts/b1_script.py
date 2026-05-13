74a8588cb8b1a4729f4fc2f5ba393fc0bda0feba
U220709704 <kamalmialqedra@posta.mu.edu.tr>
2026-05-13 17:34:32 +0300

    refactoring b1 script to use runner

--- scripts/b1_script.py
+++ scripts/b1_script.py
@@ -1,32 +1,16 @@
 import torch
 import torch.nn as nn
 from torch.optim import AdamW
-
 from data import GROUP_LABELS
-from data.data_loader import get_data_loader
-from data.transformers import get_transform
 from models.b1_model import B1Model
-from scripts import pkl_path, videos_path, device, save_path
-from utils.evaluator import full_evaluation
-from utils.trainer import train
+from scripts import device
+from utils.runner import run
 
 batch_size = 128
 num_workers = 4
-lr = 1e-3
+lr = 1e-4
 CLASS_NAMES = GROUP_LABELS.keys()
 
-frame_transform, crop_transform = get_transform()
-train_loader, val_loader, test_loader = get_data_loader(
-    pkl_path=pkl_path,
-    videos_path=videos_path,
-    mode="frame",
-    frame_transform=frame_transform,
-    batch_size=batch_size,
-    num_workers=num_workers,
-    crop_transform=crop_transform,
-)
-
-# ── Model / Loss / Optimizer ─────────────────────────────────────────────
 model = B1Model(num_classes=8)
 model = model.to(device)
 if torch.cuda.device_count() > 1:
@@ -40,24 +24,17 @@ scheduler = torch.optim.lr_scheduler.ReduceLROnPlateau(
     optimizer,
     mode="min",
     factor=0.5,
-    patience=3
+    patience=5
 )
 
 if __name__ == "__main__":
-    model, history = train(
-        model,
-        train_loader,
-        val_loader,
-        test_loader,
-        criterion,
-        optimizer,
-        CLASS_NAMES,
-        scheduler,
-        30,
-        save_path, )
-
-    full_evaluation(model, test_loader,
-                    criterion,
-                    device=device,
-                    class_names=CLASS_NAMES,
-                    cm_save_path=f"{save_path}confusion_matrix.png")
+    run(
+        model=model,
+        mode="frame",
+        num_epochs=30,
+        batch_size=128,
+        criterion=criterion,
+        optimizer=optimizer,
+        scheduler=scheduler,
+        class_names=CLASS_NAMES,
+        cm_filename="confusion_matrix_b1.png")
