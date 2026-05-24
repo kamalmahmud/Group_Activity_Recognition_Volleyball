8f138e7d6e2b940cfaef1093c0c4b7e67fea214e
U220709704 <kamalmialqedra@posta.mu.edu.tr>
2026-05-24 16:22:03 +0300

    update: b6 script

--- scripts/b6_script.py
+++ scripts/b6_script.py
@@ -24,8 +24,8 @@ if __name__ == "__main__":
     run(
         model=model,
         mode="temporal_person_clip",
-        num_epochs=20,
-        batch_size=16,
+        num_epochs=15,
+        batch_size=4,
         criterion=criterion,
         optimizer=optimizer,
         scheduler=scheduler,
