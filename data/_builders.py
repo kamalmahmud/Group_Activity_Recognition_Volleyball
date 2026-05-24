17bf961b4b5e32e7abf943a3ddd8208b11191d13
U220709704 <kamalmialqedra@posta.mu.edu.tr>
2026-05-25 00:59:28 +0300

    update:b6 model

--- data/_builders.py
+++ data/_builders.py
@@ -147,7 +147,7 @@ class DatasetIndexBuildersMixin:
         return samples
 
     def _build_temporal_person_clip_index(self, video_ids):
-        max_num = 10000
+        max_num = 250
         labels_array = [0] * 8
         samples = []
 
--- scripts/b6_script.py
+++ scripts/b6_script.py
@@ -24,10 +24,11 @@ if __name__ == "__main__":
     run(
         model=model,
         mode="temporal_person_clip",
-        num_epochs=15,
-        batch_size=4,
+        num_epochs=25,
+        batch_size=16,
         criterion=criterion,
         optimizer=optimizer,
         scheduler=scheduler,
         class_names=CLASS_NAMES,
+        num_workers=16,
         cm_filename="confusion_matrix_b6.png")
